c611898e2305d86dd97fc5d4676d803bfb76f6c4
Echmil <emilbartels3001@gmail.com>
2026-03-15 14:06:39 +0100

    laver integration tests

--- EcopointsP2/src/test/java/com/example/demo/EcoPointControllerTest.java
+++ EcopointsP2/src/test/java/com/example/demo/EcoPointControllerTest.java
@@ -4,11 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static java.lang.Math.ceil;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import org.springframework.http.MediaType;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,24 +22,42 @@ public class EcoPointControllerTest {
     private MockMvc mockMvc;
 
     @Test
+    @WithMockUser
     void getPage1() throws Exception{
-        mockMvc.perform(get("/api/ecopoint/"))
+        mockMvc.perform(get("/api/ecopoints?page=1&limit=10"))
+                .andExpect((status().isOk()))
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.pagination.page").value(1));
     }
 
 
     @Test
+    @WithMockUser
     void getPage2() throws Exception{
-
+        mockMvc.perform(get("/api/ecopoints?page=2&limit=10"))
+                .andExpect((status().isOk()))
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.pagination.page").value(2));
     }
 
     @Test
+    @WithMockUser
     void getLastPage() throws Exception{
-
+        mockMvc.perform(get("/api/ecopoints?page=1&limit=50"))
+                .andExpect((status().isOk()))
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.pagination.page").value(1));
     }
 
     @Test
     void getWithNoLatLngValuesFallback() throws Exception {
-
+        mockMvc.perform(get("/api/ecopoints")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$.pagination.page").value(1))
+                .andExpect(jsonPath("$.pagination.limit").value(10));
     }
 
 }
